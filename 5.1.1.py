a040f3a1ab11f21aebbfced9b69653e91064898d
Nur <2020975359@isiswa.uitm.edu.my>
2020-12-11 23:47:27 +0000

    Commit for 5.1.1

--- 5.1.1.py
+++ 5.1.1.py
@@ -1,21 +1,23 @@
-import socket
+import socket 
 
-s= socket.socket()
+s = socket.socket()
 print("berjaya buat socket")
 
 port = 8888
 
-s.bind((",port")
+s.bind(("",port))
 print("berjaya bind socket di port: " + str(port))
 
 s.listen(5)
-print("socket tengah menunggu client!")
+print("soket tengah menunggu client!")
 
 while True:
-	c, addr = s.accept()
-	print("dapat capaian dari: "+ str(addr))
-	
-	c.send(b'terima kasih!)
+
+	c,addr = s.accept()
+	print("dapat capaian dari: " + str(addr))
+
+	c.send(b'terima kasih!')
 	buffer = c.recv(1024)
 	print(buffer)
+
 c.close()
