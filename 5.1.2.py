2e491494c37043e9c7bc646c58fa258cd6952b2e
Nur <2020975359@isiswa.uitm.edu.my>
2020-12-11 23:59:28 +0000

    Commit for 5.1.2.py

---
+++ 5.1.2.py
@@ -0,0 +1,16 @@
+import socket
+
+s=socket.socket()
+
+#Port 
+port = 8888
+
+s.connect (('192.168.0.162',port))
+
+data = s.recv(1024)
+
+s.send(b'hi, saya client. terima kasih!');
+
+print(data)
+
+s.close()
